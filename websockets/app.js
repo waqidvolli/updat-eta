d7b9669653c450869f9ef77ab46dcc99139daac3
Waqid <wmv214@nyu.edu>
2016-09-14 23:51:56 -0400

    Implemented multi session

--- websockets/app.js
+++ websockets/app.js
@@ -3,86 +3,29 @@ var express = require("express");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
+
 app.use(express.static("./public"));
-app.get('/:name', function(req, res){
-  res.json(eval(req.params.name));
-});
+app.use('/assets',express.static('./public/assets/'));
+
 
 io.on('connection', function(socket){
-  socket.on('location', function(data){
-    io.emit('location', data);
+  socket.on(`session`, function(data){
+    console.log(`Incoming data from session ${data.sessionId}...`);
+    io.emit(`session_${data.sessionId}`, data);
   });
 });
+
+
+let sessionId = 0;
+app.get('/session/init', function(req,res){
+  sessionId++;
+  res.json(sessionId);
+});
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
 module.exports = app;
-
-
-
-//////SIMULATION DATA
-
-const A = {
-    name: 'A',
-    data: [{
-        lat: 40.804528,
-        lng: -73.957717
-    }, {
-        lat: 40.801410,
-        lng: -73.960206
-    }, {
-        lat: 40.798551,
-        lng: -73.963339
-    }, {
-        lat: 40.795725,
-        lng: -73.966215
-    }, {
-        lat: 40.794458,
-        lng: -73.971279
-    }]
-};
-
-const B = {
-    name: 'B',
-    data: [{
-        lat: 40.708536,
-        lng: -73.961019
-    }, {
-        lat: 40.712584,
-        lng: -73.968404
-    }, {
-        lat: 40.717392,
-        lng: -73.984053
-    }, {
-        lat: 40.719303,
-        lng: -73.990116
-    }, {
-        lat: 40.723011,
-        lng: -73.992948
-    }]
-};
-
-const C = {
-    name: 'C',
-    data: [{
-        lat: 40.705688,
-        lng: -74.070226
-    }, {
-        lat: 40.724504,
-        lng: -74.060504
-    }, {
-        lat: 40.733283,
-        lng: -74.049715
-    }, {
-        lat: 40.728688,
-        lng: -74.031923
-    }, {
-        lat: 40.728385,
-        lng: -74.007166
-    }]
-};
